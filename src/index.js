06d9397e81cd197c8fe1957ef1e1732d29465acf
Git-DmR <dm.r@mail.uk>
2021-02-11 19:00:28 +0300

    feat: solved reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+    n = String(n);
+    n = n.split("").reverse().join("");
+    return Number(n);
+};
